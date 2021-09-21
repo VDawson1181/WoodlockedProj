cd78344806625cc20c06a3a4bc37db97aa426027
VDawson1181 <vdawson647@gmail.com>
2021-09-21 01:55:00 -0400

    Reorg and more fixes

---
+++ scripts/WL_Con_Navigation_Control.js
@@ -0,0 +1,31 @@
+var xhr= new XMLHttpRequest();
+xhr.open('GET', 'WL_Con_NavigationController.html', true);
+xhr.onreadystatechange= function() {
+    if (this.readyState!==4) return;
+    if (this.status!==200) return; // or whatever error handling you want
+    let navContainers = document.querySelectorAll("nav");
+
+    // for(var WLNAVI=0;WLNAVI<navContainers.length;WLNAVI++){
+    //     navContainers[WLNAVI].innerHTML= this.responseText;
+    // }
+    
+    document.querySelector("#WL_navbarContainer nav").innerHTML= this.responseText;
+    // document.querySelector("#WL_ftr_navbarContainer nav").innerHTML= this.responseText;
+};
+xhr.send();
+
+var xhr= new XMLHttpRequest();
+xhr.open('GET', 'WL_Con_FooterController.html', true);
+xhr.onreadystatechange= function() {
+    if (this.readyState!==4) return;
+    if (this.status!==200) return; // or whatever error handling you want
+    let navContainers = document.querySelectorAll("#nav");
+
+    // for(var WLNAVI=0;WLNAVI<navContainers.length;WLNAVI++){
+    //     navContainers[WLNAVI].innerHTML= this.responseText;
+    // }
+    
+    // document.querySelector("#WL_navbarContainer nav").innerHTML= this.responseText;
+    document.querySelector("#WL_ftr_navbarContainer nav").innerHTML= this.responseText;
+};
+xhr.send();
